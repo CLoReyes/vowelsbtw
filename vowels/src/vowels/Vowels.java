3771dbc077c5c3598c0aebfbab72482d335ccfe4
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2018-09-04 14:48:02 -0600

    vowels sissjsjs

---
+++ vowels/src/vowels/Vowels.java
@@ -0,0 +1,38 @@
+package vowels;
+
+import java.util.Scanner;
+public class Vowels
+	{
+
+		public static void main(String[] args)
+			{
+				one();
+				two();
+				three();
+				four();
+			}
+
+		private static void one()
+			{
+				Scanner hello = new Scanner(System.in);
+				System.out.println("Guess a number between 1 and 20.");
+				int secretNumber = (int)(Math.random()10*) + 1;
+				i
+			}
+
+		private static void two()
+			{
+				
+			}
+
+		private static void three()
+			{
+				
+			}
+
+		private static void four()
+			{
+				
+			}
+
+	}
